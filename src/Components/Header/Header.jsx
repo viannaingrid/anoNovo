c5d7c848d1137fff5449419ba7966f5d6af13df3
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-14 10:24:44 -0300

    :wrench: update header with right config

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,9 @@
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import Paz from '../../Pages/Paz/';
-import Alegria from '../../Pages/Alegria';
-import Saude from '../../Pages/Saude';
 import logoAnoNovo from '../../assets/logoAnoNovo.png';
 import s from './Header/header.module.scss';
 
 
 export default function Header() {
     return (
-        <BrowserRouter>
         <header>
             <section>
                 <img src={logoAnoNovo} alt="Imagem de um champagne estourando com uma taça cheia e escrito feliz ano novo" />
@@ -16,23 +11,16 @@ export default function Header() {
             <nav>
                 <ul>
                     <li>
-                        <Link className={s.linkHeader} to="/" >Paz</Link>
+                        <a className={s.linkHeader}  >Paz</a>
                     </li>
                     <li>
-                        <Link className={s.linkHeader} to="/Alegria" >Alegria</Link>
+                        <a className={s.linkHeader}  >Alegria</a>
                     </li>
                     <li>
-                        <Link className={s.linkHeader} to="/Saude" >Saúde</Link>
+                        <a className={s.linkHeader}  >Saúde</a>
                     </li>
                 </ul>
             </nav>
         </header>
-        
-        <Routes>
-            <Route path='/' element={<Paz/>}/>
-            <Route path='/Alegria' element={<Alegria/>}/>
-            <Route path='/Saude' element={<Saude/>}/>
-        </Routes>
-        </BrowserRouter>
     )
 }
