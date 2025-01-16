5c70568cbf0884f5ede791a22fd03ae2d905c23b
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-16 16:43:46 -0300

    :wrench: config app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from '../src/Components/Header/Header.jsx';
+import Footer from '../src/Components/Footer/Footer.jsx';
+import Main from '../src/Components/Main/Main.jsx';
 import '../src/globalStyle/globalStyle.scss';
 
 export default function app (){
     return(
         <section>
             <Header/>
+            <Main/>
+            <Footer/>
         </section>
     )
 }
