ea61058bde8d9f63bded8b553fc6a8d144c64c0d
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-16 17:03:28 -0300

    :construction: config main and font

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,24 @@
+
+
+export default function Main() {
+    return (
+        <section>
+            <section>
+                <h2>Boas Festas!!!</h2>
+                <h1>Felicidades!!!</h1>
+            </section>
+            <section>
+                <p>
+                Em 2025, vamos juntos programar,
+                Com mais código limpo e muito para alcançar!
+                Que o ano novo traga só funções de felicidade,
+                E que a paz seja nossa variável de realidade!
+                Sem bugs, sem falhas, com muito amor,
+                Vamos debugar os problemas com ardor!
+                Que a vida seja sempre o output de alegria,
+                o Vai Na Web te deseja um Ano Novo, que seja pura harmonia!
+                </p>
+            </section>
+        </section>
+    )
+}
