2d6b25212b6ac4185f3edded72c1423f2edf0c73
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-16 18:20:52 -0300

    :construction: update and config components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from '../src/Components/Header/Header.jsx';
+import '../src/globalStyle/globalStyle.scss';
+import s from '../src/app.module.scss';
 import Footer from '../src/Components/Footer/Footer.jsx';
 import Main from '../src/Components/Main/Main.jsx';
-import '../src/globalStyle/globalStyle.scss';
 
 export default function app (){
     return(
-        <section>
+        <section className={s.content}>
             <Header/>
             <Main/>
             <Footer/>
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,24 +1,16 @@
 import s from '../Main/main.module.scss';
+import anoNovo from '../../assets/img-anoNovo.png';
 
 export default function Main() {
     return (
-        <section className={s.secMain} >
-            <section className={s.titleMain} >
-                <h2>Boas Festas!!!</h2>
-                <h1>Felicidades!!!</h1>
-            </section>
-            <section className={s.textMain} >
-                <p>
-                Em 2025, vamos juntos programar,
-                Com mais código limpo e muito para alcançar!
-                Que o ano novo traga só funções de felicidade,
-                E que a paz seja nossa variável de realidade!
-                Sem bugs, sem falhas, com muito amor,
-                Vamos debugar os problemas com ardor!
-                Que a vida seja sempre o output de alegria,
-                o Vai Na Web te deseja um Ano Novo, que seja pura harmonia!
-                </p>
-            </section>
-        </section>
+        <main className={s.mainContent}>
+        <img src={anoNovo} alt="Imagem ANO NOVO 2025" />
+        <div className={s.textos}>
+            <p className={s.boasFestas}>Boas Festas!!!</p>
+            <p className={s.felicidades}>Felicidades!!!</p>
+            <p className={s.mensagem2025}>Em 2025, vamos juntos programar,Com mais código limpo e muito para alcançar!Que o ano novo traga só funções de felicidade,E que a paz seja nossa variável de realidade!Sem bugs, sem falhas, com muito amor,Vamos debugar os problemas com ardor!Que a vida seja sempre o output de alegria,o Vai Na Web te deseja um Ano Novo, que seja pura harmonia!
+            </p>
+        </div>
+    </main>
     )
 }
