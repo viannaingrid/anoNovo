3a16e15ae50ca88414ee47481f19cb48cdec4aa1
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-17 13:00:44 -0300

    :wrench: config footer and tag ancor

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
+import logoFacebook from '../../assets/logoFacebook.png';
+import logoInstagram from '../../assets/logoInstagram.png';
+import logoLinkedin from '../../assets/logoLinkedin.png';
+import s from '../Footer/footer.module.scss';
+
 export default function Footer() {
     return(
-        <>
-        <h2>teste</h2>
-        </>
+        <section className={s.footerSection}>
+            <a href="https://www.instagram.com/" target="_blank"><img src={logoInstagram} alt="Logo do Instagram"/></a>
+            <a href="https://www.facebook.com/" target="_blank"><img src={logoFacebook} alt="Logo do Facebook"/></a>
+            <a href="https://www.linkedin.com/in/ingrid-vianna/" target="_blank"><img src={logoLinkedin} alt="Logo do Linkedin"/></a>
+        </section>
     )
 }
